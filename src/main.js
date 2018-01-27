6e13c2ff9f144d491dd4b0299a5b44635ce691f5
3442853561 <3442853561@qq.com>
2018-01-27 16:24:23 +0800

    updata

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 var mem = new WebAssembly.Memory({initial:10});
+
 function importObject() {
-  this.std = {
-    debug: function (index, length) {
-      let bytes = new Uint8Array(mem.buffer, index, length);
-      let str = new TextDecoder('utf8').decode(bytes);
-      console.log(str);
-    },
-    memory: mem
-  };
+  this.std = new std();
 }
 
+function std() {
+  this.debug = function (index, length) {
+    let bytes = new Uint8Array(mem.buffer, index, length);
+    let str = new TextDecoder('utf8').decode(bytes);
+    console.log(str);
+  };
+  this.memory = mem;
+}
